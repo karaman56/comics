54caf1e6d746a6224dc1cb1a08c866c5dc11c4af
karaman56 <166434324+karaman56@users.noreply.github.com>
2024-12-27 23:36:11 +0500

    Выделил отдельную функцию print
    Выделил отдельную функцию print

--- main.py
+++ main.py
@@ -6,23 +6,19 @@ import random
 
 MAX_COMIC_NUMBER = 3028
 
-
 def generate_comic_url():
     random_number = random.randint(1, MAX_COMIC_NUMBER)
     url = f'https://xkcd.com/{random_number}/info.0.json'
     return url
-
 
 def get_comic_data(url):
     response = requests.get(url)
     response.raise_for_status()
     return response.json()
 
-
 def create_comic_folder(folder_name='comics'):
     os.makedirs(folder_name, exist_ok=True)
     return folder_name
-
 
 def download_comic(comic_image_url, save_path):
     response = requests.get(comic_image_url, stream=True)
@@ -30,17 +26,14 @@ def download_comic(comic_image_url, save_path):
     with open(save_path, 'wb') as file:
         file.write(response.content)
 
-
 def send_comic_message(bot, telegram_channel_id, title, image_path, alt_text):
     bot.send_message(chat_id=telegram_channel_id, text=title)
     with open(image_path, 'rb') as photo:
         bot.send_photo(chat_id=telegram_channel_id, photo=photo)
     bot.send_message(chat_id=telegram_channel_id, text=alt_text)
 
-
 def clean_up(file_path):
     os.remove(file_path)
-
 
 def send_comic(bot, telegram_channel_id):
     url = generate_comic_url()
@@ -60,8 +53,8 @@ def send_comic(bot, telegram_channel_id):
         if os.path.exists(comic_file_path):
             clean_up(comic_file_path)
 
-
-
+def print_message(message):
+    print(message)
 
 def run_bot():
     load_dotenv()
@@ -69,13 +62,13 @@ def run_bot():
     telegram_channel_id = os.getenv('TELEGRAM_CHANNEL_ID')
     bot = telebot.TeleBot(token)
 
-    print("Бот запущен. Публикуем комикс...")
+    print_message("Бот запущен. Публикуем комикс...")
     send_comic(bot, telegram_channel_id)
-    print("Комикс опубликован. Скрипт завершает работу.")
-
+    print_message("Комикс опубликован. Скрипт завершает работу.")
 
 if __name__ == "__main__":
     run_bot()
+
 
 
 
