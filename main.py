400d70c9bdc8744b61d393df624c921492c26118
karaman56 <166434324+karaman56@users.noreply.github.com>
2024-12-29 00:49:51 +0500

    Код просто офигенный )
    Код просто офигенный

--- main.py
+++ main.py
@@ -6,21 +6,12 @@ import random
 
 MAX_COMIC_NUMBER = 3028
 
-def generate_comic_url():
+def get_comic_data():
     random_number = random.randint(1, MAX_COMIC_NUMBER)
     url = f'https://xkcd.com/{random_number}/info.0.json'
-    return url
-
-
-def get_comic_data(url):
     response = requests.get(url)
     response.raise_for_status()
     return response.json()
-
-
-def create_comic_folder(folder_name='comics'):
-    os.makedirs(folder_name, exist_ok=True)
-    return folder_name
 
 
 def download_comic(comic_image_url, save_path):
@@ -37,45 +28,49 @@ def send_comic_message(bot, telegram_channel_id, title, image_path, alt_text):
     bot.send_message(chat_id=telegram_channel_id, text=alt_text)
 
 
-def clean_up(file_path):
-    os.remove(file_path)
-
-
-def send_comic(bot, telegram_channel_id):
-    url = generate_comic_url()
-    comic_data = get_comic_data(url)
-
+def fetch_and_download_comic():
+    comic_data = get_comic_data()
     comic_image_url = comic_data.get('img')
     comic_image_title = comic_data.get('title')
     comic_image_alt = comic_data.get('alt')
-
-    comic_folder = create_comic_folder()
+    comic_folder = 'comics'
+    os.makedirs(comic_folder, exist_ok=True)
     comic_file_path = os.path.join(comic_folder, f"{comic_image_title}.png")
 
+    download_comic(comic_image_url, comic_file_path)
+
+    return comic_image_title, comic_file_path, comic_image_alt
+
+
+def send_comic(bot, telegram_channel_id):
+    comic_image_title, comic_file_path, comic_image_alt = fetch_and_download_comic()
     try:
-        download_comic(comic_image_url, comic_file_path)
         send_comic_message(bot, telegram_channel_id, comic_image_title, comic_file_path, comic_image_alt)
     finally:
         if os.path.exists(comic_file_path):
-            clean_up(comic_file_path)
-
-
-def print_message(message):
-    print(message)
+            os.remove(comic_file_path)
 
 
 def run_bot():
     load_dotenv()
-    token = os.getenv('TELEGRAM_BOT_TOKEN')
-    telegram_channel_id = os.getenv('TELEGRAM_CHANNEL_ID')
+
+    try:
+        token = os.environ['TELEGRAM_BOT_TOKEN']
+        telegram_channel_id = os.environ['TELEGRAM_CHANNEL_ID']
+    except KeyError as e:
+        raise RuntimeError(f"переменная окружения не установлена: {e}")
+
     bot = telebot.TeleBot(token)
 
-    print_message("Бот запущен. Публикуем комикс...")
+    print("Бот запущен.")
     send_comic(bot, telegram_channel_id)
-    print_message("Комикс опубликован. Скрипт завершает работу.")
+    print("Комикс опубликован.")
+
 
 if __name__ == "__main__":
     run_bot()
+
+
 
 
 
